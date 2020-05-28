4447083d6b33ef633ef9bf9268c89677927487ef
sjgeyman <62623550+sjgeyman@users.noreply.github.com>
2020-05-27 20:57:51 -0700

    Update script.js

--- script.js
+++ script.js
@@ -39,7 +39,7 @@
       });
 
     myConnector.getData = function (table, doneCallback) {
-        $.getJSON("https://secure.fusebill.com/v1/customers", function(resp) {
+        $.getJSON("http://secure.fusebill.com/v1/customers", function(resp) {
         var feat = resp.features,
             tableData = [];
 
